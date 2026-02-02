308e303ab4d9cafa4394b4d0f682c566f25dc32e
Fin Diepman <shadowyeet23@gmail.com>
2026-02-02 12:26:28 +0100

    Added register and login functions

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -3,6 +3,32 @@ import { User, Session } from "./types";
 import { state } from "../state";
 
 function registerUser(username: string, password: string) {
-    const existingUser = state.users.find(user)
-    if (!username.value && )
+    const existingUser = state.users.find(user => user.username === username.toLowerCase());
+    if (!existingUser && username && password) {
+        const newUser = {
+            username: username.toLowerCase(),
+            password: password,
+            id: Date.now().toString()
+        }
+        state.users.push(newUser)
+        saveToStorage('users', state.users)
+    }
+}
+
+function loginUser(username: string, password: string) {
+    if (username.trim() !== "" && password.trim() !== "") {
+        const existingUser = state.users.find(user => user.username === username.toLowerCase())
+        if (!existingUser) {
+            return 'invalid username';
+        }
+        else if (existingUser.password !== password) {
+            return 'invalid password'
+        }
+        else {
+            state.session = {userId: existingUser.id, createdAt: Date.now()}
+            saveToStorage('session', state.session)
+            return 'Succesfully logged in'
+        }
+    }
+    return 'empty fields'
 }
